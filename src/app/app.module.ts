8e6be3cd31d2a663c3de47c62ff57adec6e4dd79
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-08-07 13:17:12 +0200

    configure Mongoose/config modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ cache: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const options: MongooseModuleOptions = {
+          uri: 'mongodb+srv://Kiran:wLY7y5ZllgfYp3gL@fittrackr-cluster.nxyaxj2.mongodb.net/?retryWrites=true&w=majority',
+        };
+        return options;
+      },
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+  controllers: [],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}

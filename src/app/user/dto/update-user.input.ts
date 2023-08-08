993d7b6978f9c59717e17ef0419cca93fba5459b
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-08-08 15:59:48 +0200

    define user entity, resolvers and dtos

--- src/app/user/dto/update-user.input.ts
+++ src/app/user/dto/update-user.input.ts
@@ -3,6 +3,12 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateUserInput extends PartialType(CreateUserInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => String)
+  name: string;
+
+  @Field(() => String)
+  email: string;
+
+  @Field(() => String)
+  password: string;
 }
--- src/app/user/entities/user.entity.ts
+++ src/app/user/entities/user.entity.ts
@@ -1,7 +1,24 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Schema as MongooseSchema } from 'mongoose';
 
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Field(() => String)
+  @Prop()
+  name: string;
+
+  @Field(() => String)
+  @Prop({ unique: true, nullable: true })
+  email?: string;
+
+  @Field(() => String)
+  @Prop()
+  password?: string;
 }
+
+export type UserDocument = User & Document;
+export const UserSchema = SchemaFactory.createForClass(User);

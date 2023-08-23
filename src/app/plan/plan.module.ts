c9345c7479f8270a1867f3bb9fedbf03b529e221
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-08-23 11:30:24 +0200

    add planschema for imports in module

--- src/app/plan/plan.module.ts
+++ src/app/plan/plan.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlanService } from './plan.service';
 import { PlanResolver } from './plan.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PlanSchema } from './entities/plan.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Plan', schema: PlanSchema }])],
   providers: [PlanResolver, PlanService],
 })
 export class PlanModule {}

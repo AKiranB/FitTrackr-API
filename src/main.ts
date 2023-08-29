787545566c602bb85e9cc039f6bb25983df1beee
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2023-08-29 12:24:38 +0200

    add port to env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //TODO add env var for port below
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
